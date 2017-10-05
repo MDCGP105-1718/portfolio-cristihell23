7ebdc2dc35b1859ca8b133cb6ed7d44e2f5f4e9a
Cristian-Emil Hanganu <cristihell23@yahoo.com>
2017-10-05 10:31:30 +0100

    second version of ex5.py

--- ex5.py
+++ ex5.py
@@ -1,2 +1,5 @@
 for n in range (99, 0, -1):
-    print (f"{n} bottles of beer on the wall, {n} bottles of beer.")
+    print (f"{n} bottles of beer on the wall, {n} bottles of beer.)
+    if (f"{n}) 0 > 1)
+    print ("No more bottles of beer on the wall, no more bottles of beer.")
+    print ("Go to the store and buy some more, 99 bottles of beer on the wall…")
